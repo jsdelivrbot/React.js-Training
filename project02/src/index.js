85bc211af91cc584d5c5a4e1306121c63c65fc16
Rafal Ozog <rafalozog@gmail.com>
2018-07-06 11:31:43 +0100

    Training repo upload

---
+++ project02/src/index.js
@@ -0,0 +1,21 @@
+/*function sayHello() {
+    console.log('chuje');
+}
+
+sayHello();
+
+const person = {
+    name: 'Rafal',
+    sayChuj() { console.log('No mowie!!') },
+    showThis() { console.log(this) }
+};
+
+person.showThis();*/
+
+const first = [1, 2, 3];
+const clone = [...first];
+
+first[1] = 5;
+
+console.log(first);
+console.log(clone);
